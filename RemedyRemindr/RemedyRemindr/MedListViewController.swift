ec74ea35e7eef7e889c10892087abcf58b8fe949
Tony <tony@w134-87-186-048.wireless.uvic.ca>
2015-02-11 15:10:48 -0800

    Main screen fully replaced with view.

--- RemedyRemindr/RemedyRemindr/MedListViewController.swift
+++ RemedyRemindr/RemedyRemindr/MedListViewController.swift
@@ -8,19 +8,82 @@
 
 import UIKit
 
-class MedListViewController: UIViewController {
+class MedListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var meds = [Medication]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func unwindToMain(sender: UIStoryboardSegue) {
+        // This comes from the detail view when you press the delete button
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        meds = MedicationDAO.getMedications()!
+        
+        println("refreshing view")
+        for med1 in meds {
+            println("med")
+            println(med1.name)
+            println(med1.reminders)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Table view data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return meds.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MedicationCell", forIndexPath: indexPath) as UITableViewCell
+        
+        print(indexPath.row)
+        
+        // Configure the cell...
+        let med = meds[indexPath.row]
+        cell.textLabel?.text = med.name
+        cell.detailTextLabel?.text = med.name
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "showDetails"
+        {
+            var detailsView : MedDetailsViewController = segue.destinationViewController as MedDetailsViewController
+            
+            var indexPath = self.tableView.indexPathForSelectedRow()
+            let med = meds[indexPath!.row]
+            
+            detailsView.inputMed = med
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
